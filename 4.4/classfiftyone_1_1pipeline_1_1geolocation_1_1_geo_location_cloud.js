732d4cbe6daaf8a2075835d0257c646f208412a9
Automation51D <51DCI@51Degrees.com>
2023-11-10 03:30:40 +0000

    REF: Updated documentation.

--- 4.4/classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.js
+++ 4.4/classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.js
@@ -1,5 +1,5 @@
 var classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud =
 [
     [ "__construct", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.html#a7b2a9988814b14bb179ca07324c34979", null ],
-    [ "onRegistration", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.html#a8d8f01d58ee96f3f9c638415b85ddc51", null ]
+    [ "processInternal", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.html#a305b9c5efa93b9c31d77c8e3600db7f3", null ]
 ];
