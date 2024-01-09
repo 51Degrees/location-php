95c78d6eeaf4afc36380ccaf7c9319d79e9b4a43
Automation51D <51DCI@51Degrees.com>
2024-01-09 03:30:46 +0000

    REF: Updated documentation.

--- 4.4/classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.js
+++ 4.4/classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.js
@@ -1,5 +1,5 @@
 var classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud =
 [
-    [ "__construct", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.html#ab2e12d5931bf3e490dd92487204093e5", null ],
-    [ "processInternal", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.html#a305b9c5efa93b9c31d77c8e3600db7f3", null ]
+    [ "__construct", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.html#a84014b7904e4b7e0232a850934678a6b", null ],
+    [ "processInternal", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.html#a7567a4db75c7e52429c1595f51875571", null ]
 ];
--- 4.4/classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.js
+++ 4.4/classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.js
@@ -1,8 +1,8 @@
 var classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder =
 [
-    [ "__construct", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.html#ac438738c0431096ef2fad5c8c2e10aeb", null ],
+    [ "__construct", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.html#a8386128db9c0e8e3360bee0456012cf7", null ],
     [ "$cache", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.html#a895139e8ec49a7b3d941e63c0d6da4e5", null ],
-    [ "$licenseKey", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.html#ac17e05ed0cfa129642f415a6a85fd05a", null ],
-    [ "$resourceKey", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.html#a3d5ef23aa0ab6199c275d82f2e90d3d2", null ],
-    [ "$restrictedProperties", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.html#ac13eab750ad4d52c21e438387b934ae7", null ]
+    [ "$licenseKey", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.html#aae6db596ff430b073e632b0f748a94ff", null ],
+    [ "$resourceKey", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.html#a5c2f7c4881815cfca58401712b29e158", null ],
+    [ "$restrictedProperties", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.html#ad7326c0268e5407fc3b56c4b49eaaa2b", null ]
 ];
