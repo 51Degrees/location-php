c9efa152470a28afae9111a9116d0cc574eac863
Automation51D <51DCI@51Degrees.com>
2024-01-25 03:30:55 +0000

    REF: Updated documentation.

--- 4.4/annotated.js
+++ 4.4/annotated.js
@@ -3,6 +3,13 @@ var annotated =
     [ "fiftyone", null, [
       [ "pipeline", null, [
         [ "geolocation", null, [
+          [ "examples", null, [
+            [ "cloud", null, [
+              [ "classes", null, [
+                [ "Constants", "classfiftyone_1_1pipeline_1_1geolocation_1_1examples_1_1cloud_1_1classes_1_1_constants.html", "classfiftyone_1_1pipeline_1_1geolocation_1_1examples_1_1cloud_1_1classes_1_1_constants" ]
+              ] ]
+            ] ]
+          ] ],
           [ "GeoLocationCloud", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud.html", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_cloud" ],
           [ "GeoLocationPipelineBuilder", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder.html", "classfiftyone_1_1pipeline_1_1geolocation_1_1_geo_location_pipeline_builder" ]
         ] ]
---
+++ 4.4/classfiftyone_1_1pipeline_1_1geolocation_1_1examples_1_1cloud_1_1classes_1_1_constants.js
@@ -0,0 +1,4 @@
+var classfiftyone_1_1pipeline_1_1geolocation_1_1examples_1_1cloud_1_1classes_1_1_constants =
+[
+    [ "RESOURCE_KEY_ENV_VAR", "classfiftyone_1_1pipeline_1_1geolocation_1_1examples_1_1cloud_1_1classes_1_1_constants.html#a41dc95341f8ef3cc039d7c2345d1a4fb", null ]
+];
